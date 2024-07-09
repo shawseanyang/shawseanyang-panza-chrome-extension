ee9a3673b03b0db4efaa40c345d4f07c63f05551
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2024-07-09 17:14:26 +0200

    add panza button that queries a panza server on localhost

--- src/content.js
+++ src/content.js
@@ -1,15 +1,51 @@
 import * as InboxSDK from '@inboxsdk/core';
 
+const PANZA_API_URL = 'http://localhost:5000/generate';
+
+async function get_panza_suggestion(text) {
+  return await fetch(PANZA_API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({text: text}),
+  })
+  .then(response => response.json())
+  .then(data => {
+    return data.generated_text;
+  });
+}
+
 InboxSDK.load(2, "sdk_panza_4e702e3f8e").then((sdk) => {
-  // the SDK has been loaded, now do something with it!
   sdk.Compose.registerComposeViewHandler((composeView) => {
-    // a compose view has come into existence, do something with it!
-    composeView.addButton({
-      title: "My Nifty Button!",
-      iconUrl:
-        "https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365",
+    let loadingStartTime;
+    let loadingInterval;
+    let button = composeView.addButton({
+      title: "Panza",
+      iconUrl: 'https://cdn4.iconfinder.com/data/icons/animal-kingdom/160/lama-512.png',
+      hasDropdown: true,
       onClick(event) {
-        event.composeView.insertTextIntoBodyAtCursor("Hello World!");
+        loadingStartTime = Date.now();
+        loadingInterval = setInterval(() => {
+          const loadingTime = ((Date.now() - loadingStartTime) / 1000).toFixed(2);
+          event.dropdown.el.innerHTML = `Loading... ${loadingTime} seconds elapsed`;
+        }, 1000);
+        const text = composeView.getTextContent();
+        get_panza_suggestion(text).then((suggestion) => {
+          const loadingTime = ((Date.now() - loadingStartTime) / 1000).toFixed(2);
+          event.dropdown.el.innerHTML = `Loaded in ${loadingTime} seconds`;
+          composeView.setBodyText(suggestion);
+        })
+        .catch((error) => {
+          event.dropdown.el.innerHTML = `Error: ${error}`;
+        })
+        .finally(() => {
+          clearInterval(loadingInterval);
+        })
+      },
+      dropdown: {
+        el: document.createElement('div'),
+        persistOnClick: true,
       },
     });
   });
