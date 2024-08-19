5fcf0a654d38c055502c5a252dfe0ad3b83f834f
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2024-08-19 21:32:50 +0200

    change to streaming

--- src/content.js
+++ src/content.js
@@ -1,24 +1,31 @@
 import * as InboxSDK from '@inboxsdk/core';
 
-const PANZA_API_URL = 'http://localhost:5000/generate';
+const PANZA_API_URL = 'http://localhost:5001/generate';
 
-async function get_panza_suggestion(text) {
-  return await fetch(PANZA_API_URL, {
+async function getPanzaSuggestionStreamAndUpdate({input, onUpdate, onComplete}) {
+  console.log("fetching panza suggestion");
+  const response = await fetch(PANZA_API_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'x-api-key': process.env.API_KEY,
     },
-    body: JSON.stringify({text: text}),
+    body: JSON.stringify({text: input}),
   })
-  .then(response => response.json())
-  .then(data => {
-    if (data.error) {
-      throw new Error(data.error);
-    }
-    return data;
-  })
-  .then(data => data.generated_text)
+  
+  const reader = response.body.getReader();
+  const decoder = new TextDecoder('utf-8');
+  let result;
+  let textSoFar = '';
+
+  while (!result?.done) {
+      result = await reader.read();
+      const chunk = decoder.decode(result.value || new Uint8Array(), { stream: !result.done });
+      textSoFar += chunk;
+      onUpdate(textSoFar);
+  }
+
+  onComplete();
 }
 
 function toHtml(text) {
@@ -27,37 +34,48 @@ function toHtml(text) {
   return `<div>${text}</div>`;
 }
 
+const PanzaButton = ({composeView}) => {
+
+  // Button state
+  let loadingStartTime;
+  let loadingInterval;
+  let loadingTime;
+
+  // Event handlers
+  const panzaResponseOnUpdate = (out) => composeView.setBodyHTML(toHtml(out));
+  const panzaResponseOnComplete = () => {
+    clearInterval(loadingInterval);
+    panzaButton.dropdown.el.innerHTML = `Loaded in ${loadingTime} seconds`;
+  }
+  const panzaButtonOnClick = (event) => {
+    loadingStartTime = Date.now();
+    loadingInterval = setInterval(() => {
+      loadingTime = ((Date.now() - loadingStartTime) / 1000).toFixed(2);
+      event.dropdown.el.innerHTML = `Loading... ${loadingTime} seconds elapsed`;
+    }, 1000);
+    const input = composeView.getTextContent();
+    getPanzaSuggestionStreamAndUpdate({
+      input: input,
+      onUpdate: panzaResponseOnUpdate,
+      onComplete: panzaResponseOnComplete,
+    })
+  };
+
+  // Button configuration
+  return {
+    title: "Panza",
+    iconUrl: 'https://i.imgur.com/w6NxSwi.png',
+    hasDropdown: true,
+    onClick: panzaButtonOnClick,
+    dropdown: {
+      el: document.createElement('div'),
+      persistOnClick: true,
+    },
+  }
+};
+
 InboxSDK.load(2, "sdk_panza_4e702e3f8e").then((sdk) => {
   sdk.Compose.registerComposeViewHandler((composeView) => {
-    let loadingStartTime;
-    let loadingInterval;
-    let button = composeView.addButton({
-      title: "Panza",
-      iconUrl: 'https://i.imgur.com/w6NxSwi.png',
-      hasDropdown: true,
-      onClick(event) {
-        loadingStartTime = Date.now();
-        loadingInterval = setInterval(() => {
-          const loadingTime = ((Date.now() - loadingStartTime) / 1000).toFixed(2);
-          event.dropdown.el.innerHTML = `Loading... ${loadingTime} seconds elapsed`;
-        }, 1000);
-        const text = composeView.getTextContent();
-        get_panza_suggestion(text).then((suggestion) => {
-          const loadingTime = ((Date.now() - loadingStartTime) / 1000).toFixed(2);
-          event.dropdown.el.innerHTML = `Loaded in ${loadingTime} seconds`;
-          composeView.setBodyHTML(toHtml(suggestion));
-        })
-        .catch((error) => {
-          event.dropdown.el.innerHTML = `Error: ${error}`;
-        })
-        .finally(() => {
-          clearInterval(loadingInterval);
-        })
-      },
-      dropdown: {
-        el: document.createElement('div'),
-        persistOnClick: true,
-      },
-    });
+    composeView.addButton(PanzaButton({composeView: composeView}));
   });
 });
